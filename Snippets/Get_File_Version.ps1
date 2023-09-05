ce1e71af61d6d6fd0790bf20be53284478098a23
Kim Bum Jun <admin@vivabj.com>
2023-09-05 20:40:14 +0900

    Add Snippets Files

---
+++ Snippets/Get_File_Version.ps1
@@ -0,0 +1,17 @@
+﻿param(
+    [string]$DLL
+)
+ 
+if (!(Test-Path $DLL)) {
+    throw "File '{0}' does not exist" -f $DLL
+}
+ 
+try {
+    $version =
+        Get-ChildItem $DLL | Select-Object -ExpandProperty VersionInfo |
+            Select-Object FileVersion | Select-Object -ExpandProperty FileVersion
+ 
+    return $version
+} catch {
+    throw "Failed to get DLL file version: {0}." -f $_
+}
---
+++ Snippets/New-ManyFiles.ps1
@@ -0,0 +1,1 @@
+﻿1..999 | foreach-object { New-Item -Path $("F:\Temp\Files\vivFile_{0:000}.txt" -f  $_)}
