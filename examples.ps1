3bec7d20f401a029a9b2d0eb6641dcd326f7eba4
Kim Bum Jun <admin@vivabj.com>
2024-01-25 21:53:06 +0900

    add examples.ps1

---
+++ examples.ps1
@@ -0,0 +1,14 @@
+﻿
+try {
+    Get-Content '~/Temp/helloworld@$@#$.cs' -ErrorAction Stop
+}
+catch [System.IO.IOException]{
+    Write-Host "Something went wrong"
+}
+catch {
+    Write-Host "Second Catch"
+}
+finally {
+    # Clean up resources.
+    Write-Host "The end"
+}
