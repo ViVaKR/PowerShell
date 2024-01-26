8d275bb68e90ccb9ecdea689cabc9dacf650aa14
Kim Bum Jun <admin@vivabj.com>
2024-01-26 21:32:34 +0900

    new Windows-SSH

---
+++ Snippets/Windows-SSH.ps1
@@ -0,0 +1,8 @@
+﻿
+Get-WindowsCapability -Online | Where-Object Name -like 'OpenSSH*'
+
+# Install the OpenSSH Client
+Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
+
+# Install the OpenSSH Server
+Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
