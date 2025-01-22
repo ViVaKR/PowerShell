4ba6e9650cfccd4cc5f7cf90816cf5f1de8aa576
Kim Bum Jun <admin@vivabj.com>
2025-01-22 23:44:39 +0900

    Create sqlserver

---
+++ Caskod/backup-db.ps1
@@ -0,0 +1,18 @@
+param (
+    [string]$db 
+)
+
+$server = "192.168.0.4,59273"
+$database = $db
+$username = "SA"
+$password = "Pass!word@"
+$Y = (Get-Date).ToString("yyyy")
+$M = (Get-Date).ToString("MM")
+$D = (Get-Date).ToString("dd")
+$rnd = Get-Random -Minimum 10000 -Maximum 99999
+# $query = "Backup Database " + $db +" TO DISK='C:\SQLServer\Backup\" + $db + "_" + $Y + "_" + $M + "_" + $D + "_" + $rnd + ".bak'"
+$query = "Backup Database ${db} TO DISK='C:\SQLServer\Backup\${db}_${Y}_${M}_${D}_${rnd}.bak'"
+$connectionString = "Server=$server;Database=$database;User Id=$username;Password=$password;Encrypt=Optional;"
+Invoke-Sqlcmd -ConnectionString $connectionString -Query $query
+
+ls C:\SQLServer\Backup
---
+++ Caskod/mssql.ps1
@@ -0,0 +1,13 @@
+param (
+    [string]$IpAddress
+)
+$Password = [System.Environment]::GetEnvironmentVariable("SA_PASSWORD", [System.EnvironmentVariableTarget]::User)
+sqlcmd -S $IpAddress -U SA -d master -P $Password -y 30 -Y 30
+
+
+
+# Set / Get, Global Variable
+# Set --> [System.Environment]::SetEnvironmentVariable("SA_PASSWORD", "Pass@word!", [System.EnvironmentVariableTarget]::User)
+# Get --> $Password = [System.Environment]::GetEnvironmentVariable("SA_PASSWORD", [System.EnvironmentVariableTarget]::User)
+# 명령어는 사용자 환경 변수로 "SA_PASSWORD"를 설정합니다. 이 설정은 레지스트리의 HKEY_CURRENT_USER\Environment 경로에 저장됩니다.
+# Read Reg --> Get-ItemProperty -Path "HKCU:\Environment"
