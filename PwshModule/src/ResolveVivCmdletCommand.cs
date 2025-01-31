4a80762dfd1b1e1e2e5ebd73416b3a602bc3111a
Kim Bum Jun <admin@vivabj.com>
2025-01-31 20:29:31 +0900

    Create new PwshModule

---
+++ PwshModule/src/ResolveVivCmdletCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Management.Automation;
+namespace PwshModule;
+
+[Cmdlet(VerbsDiagnostic.Resolve, "VivCmdlet")]
+public class ResolveVivCmdletCommand : PSCmdlet
+{
+    [Parameter(Position=0)]
+    public Object? InputObject { get;set;}
+    
+    protected override void EndProcessing() 
+    {
+        this.WriteObject(this.InputObject);
+        base.EndProcessing();
+    }
+}
